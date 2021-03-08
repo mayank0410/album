8abe64bbec896996c1c55ede30067f841b93f36b
Mayank Singh <65878632+mayank0410@users.noreply.github.com>
2021-03-09 01:51:41 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// document.querySelector('#sub1').addEventListener('click',function(){
+//     if (!document.querySelector('#name').value=='' && !document.querySelector('#mail').value=='' && !document.querySelector('#message').value=='') {
+//         alert('Thanks for your message...')
+//     }
+
+// })
+
+document.querySelector('#sub2').addEventListener('click',function(){
+    if (!document.querySelector('#fname').value=='' && !document.querySelector('#lname').value=='' && !document.querySelector('#mail').value=='' && !document.querySelector('#mobile').value=='') {
+        alert('Thanks for booking, will share confirmation on mail soon.')
+    }
+
+})
